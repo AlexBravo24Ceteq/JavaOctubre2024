837fb6d779bffdb1b62ba7a8c589c0144f7c5be6
GLPZ3K <100618578+GLPZ3K@users.noreply.github.com>
2024-10-18 13:02:21 -0600

    Delete SegundaSemanaObjetos_GLP directory

--- SegundaSemanaObjetos_GLP/src/com/Abstraccion.java
+++
@@ -1,8 +0,0 @@
-package com;
-
-public class Abstraccion {
-
-	//Abstraccion
-	//Son los atributos que tiene una clase en programacion orientado a objetos
-	//ejemplo de carro: llantas, volante, motor, etc
-}
