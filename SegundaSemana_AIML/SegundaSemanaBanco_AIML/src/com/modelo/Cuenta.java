854fc1df10a7d29a959b27536f011281de3535e7
aiml1985 <armando.isaac1985@gmail.com>
2024-10-21 20:04:09 -0600

    Ejercicio Prueba AIML

---
+++ SegundaSemana_AIML/SegundaSemanaBanco_AIML/src/com/modelo/Cuenta.java
@@ -0,0 +1,20 @@
+package com.modelo;
+
+public class Cuenta {
+	
+	//En el paquete com.modelo guardamos la informacion
+	//de los obejetos o entidades a los cuales 
+	//queremos acceder
+	//En este ejemplo necesitamos crear varias cuentas
+	//de usuarios y las agregaremos a un HashMap para
+	//simular una pequeña BD
+	
+	private String usuario;
+	private double saldo;
+	private double saldoMin;
+	private double saldoMax;
+	private String tipoCuenta;
+	
+	prublic Cuenta () {}  
+ 
+}
